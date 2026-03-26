78ae304da8065c436bbf185d1197bca9d3effd4e
zakki-z <zakariaeazhari@proton.me>
2026-03-26 19:56:42 +0100

    refactor: create proper code structure

--- code/backend/src/main/java/com/example/backend/BackendApplication.java
+++ code/backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package com.example.server;
+package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServerApplication {
+public class BackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServerApplication.class, args);
+        SpringApplication.run(BackendApplication.class, args);
     }
 
 }
